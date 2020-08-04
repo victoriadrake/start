25411cbf19d8b4dcc6639da906793f1ca11433a7
Victoria Drake <hello@victoria.dev>
2020-08-03 20:31:24 -0400

    🐣 Initial release

---
+++ README.md
@@ -0,0 +1,23 @@
+# start ☕
+
+A new tab startpage or home page. Shows local and UTC time, day of the week and date, and features search (via [Startpage](https://www.startpage.com/)) and bookmarks you can version control.
+
+There are three themes that change throughout the day.
+
+![Morning time](./img/morning.png)
+
+![Day time](./img/day.png)
+
+![Night time](./img/night.png)
+
+## Usage
+
+Fork this repo, change the links to your preferred sites if you like, and [set your GitHub Pages source](https://docs.github.com/en/github/working-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site).
+
+In your web browser, set the URL for new tab pages to your new start page, hosted by GitHub Pages. ✨
+
+- [Set home page or new tab page in Firefox](https://support.mozilla.org/en-US/kb/how-to-set-the-home-page)
+- [Set homepage or startup page in Chrome](https://support.google.com/chrome/answer/95314)
+- [Change your homepage in Safari on Mac](https://support.apple.com/guide/safari/change-your-homepage-ibrw1020/mac)
+
+You can also keep the page local by setting the URL to the filepath of `index.html` on your machine.
---
+++ index.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  setTime()
+  setDate()
+  setStyle()
+  setInterval(setTime, 1000)
+
+  const button = document.querySelector('.content-input > button')
+
+  button.addEventListener('click', search)
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) search()
+  })
+
+  function setTime () {
+    const now = new Date()
+    const utcTime = now.toISOString().replace('T', ' ').replace('.', ' ').substr(11, 9) + ' UTC'
+
+    document.getElementById('current-time').innerHTML = now.toLocaleTimeString()
+    document.getElementById('utc-time').innerHTML = utcTime
+  }
+
+  function setDate () {
+    const now = new Date()
+    document.getElementById('date').innerHTML = now.toDateString().substr(4)
+  }
+
+  function search () {
+    const str = document.getElementById('search').value
+
+    const output = 'https://www.startpage.com/do/dsearch?query=' + str
+
+    location.href = output
+  }
+
+  function setStyle () {
+    const now = new Date()
+    const h = now.getHours()
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const w = now.getDay()
+    const day = days[w]
+
+    const greetings = document.querySelector('.content-greeting > h1')
+
+    let period
+
+    if (h >= 5 && h < 12) period = 1
+    else if (h >= 12 && h < 18) period = 2
+    else period = 0
+
+    if (period === 1) {
+      greetings.innerHTML = 'Good morning. It\'s ' + day + '.'
+      document.querySelector(':root').style.setProperty('--background', '#DCEDFF')
+      document.querySelector(':root').style.setProperty('--input', '#fff')
+      document.querySelector(':root').style.setProperty('--link', '#4A5E6D')
+      document.querySelector(':root').style.setProperty('--shadow', '#232C33')
+      document.querySelector(':root').style.setProperty('--color', '#232C33')
+    }
+    if (period === 2) {
+      greetings.innerHTML = 'Good afternoon. It\'s ' + day + '.'
+      document.querySelector(':root').style.setProperty('--background', '#97C3B6')
+      document.querySelector(':root').style.setProperty('--input', '#F9E7E7')
+      document.querySelector(':root').style.setProperty('--color', '#0A2E36')
+      document.querySelector(':root').style.setProperty('--shadow', '#0A2E36')
+    }
+    if (period === 0) {
+      greetings.innerHTML = 'Good evening. It\'s still ' + day + '.'
+      document.querySelector(':root').style.setProperty('--background', '#161212')
+      document.querySelector(':root').style.setProperty('--input', '#7B6565')
+      document.querySelector(':root').style.setProperty('--color', '#F9E7E7')
+      document.querySelector(':root').style.setProperty('--link', '#7B6565')
+      document.querySelector(':root').style.setProperty('--hover', '#F9E7E7')
+      document.querySelector(':root').style.setProperty('--shadow', '#000')
+    }
+  }
+})
