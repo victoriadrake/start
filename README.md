6d70c9d51c07e5f4ea952a774d32782901b5c370
Victoria Drake <hello@victoria.dev>
2022-07-31 13:09:16 -0500

    Update themes and add weather

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-# start ☕
+# start
 
-A new tab startpage or home page. Shows local and UTC time, day of the week and date, and features search (via [Startpage](https://www.startpage.com/)) and bookmarks you can version control.
+A start page. Shows local and UTC time, day of the week and date, and features search (via [Startpage](https://www.startpage.com/)) and the weather (via [OpenWeather](https://openweathermap.org/)).
 
-There are three themes that change throughout the day.
-
-![Morning time](./img/morning.png)
-
-![Day time](./img/day.png)
-
-![Night time](./img/night.png)
-
-There's a bonus theme if you stay up long enough.
+![London in the afternoon](preview.png)
 
 ## Usage
 
-Fork this repo, change the links to your preferred sites if you like, and [set your GitHub Pages source](https://docs.github.com/en/github/working-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site).
+Fork this repo.
 
-In your web browser, set the URL for new tab pages to your new start page, hosted by GitHub Pages. ✨
+If you want to publicly host your page using GitHub Pages, [set your GitHub Pages source](https://docs.github.com/en/github/working-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site).
+
+In `index.js`, add your [City ID](https://openweathermap.org/current#cityid) and [OpenWeather API key](https://openweathermap.org/price#current) on lines 1 and 2.
+
+In your web browser, set the URL for new tab pages to your new start page, either hosted by GitHub Pages or to the local filepath of `index.html` on your machine.
 
 - [Set home page in Firefox](https://support.mozilla.org/en-US/kb/how-to-set-the-home-page) and [override new tab pages](https://addons.mozilla.org/en-US/firefox/addon/new-tab-override/)
 - [Set homepage or startup page](https://support.google.com/chrome/answer/95314) and [override new tab page](https://chrome.google.com/webstore/detail/new-tab-redirect/icpgjfneehieebagbmdbhnlpiopdcmna/) in Chrome
 - [Change your homepage in Safari on Mac](https://support.apple.com/guide/safari/change-your-homepage-ibrw1020/mac)
-
-You can also keep the page local by setting the URL to the filepath of `index.html` on your machine.
--- index.js
+++ index.js
@@ -1,30 +1,76 @@
+const CITY = '2643743' // Find your city ID here: https://openweathermap.org/current#cityid
+const API_KEY = 'xxx' // Add your API key
+
 document.addEventListener('DOMContentLoaded', function (event) {
-  setTime()
-  setDate()
-  setStyle()
-  setInterval(setTime, 1000)
+  // Refresh content every 30 mins
+  window.setTimeout(function () {
+    window.location.reload()
+    //   }, 30000) // 30 seconds for testing
+  }, 1800000)
 
-  const button = document.querySelector('.content-input > button')
-
-  button.addEventListener('click', search)
-  document.addEventListener('keydown', function (e) {
-    if (e.keyCode === 13) search()
-  })
-
-  function setTime () {
+  const setTime = () => {
     const now = new Date()
-    const utcTime = now.toISOString().replace('T', ' ').replace('.', ' ').substr(11, 9) + ' UTC'
+    const utcTime = now.toISOString().replace('T', ' ').replace('.', ' ').substring(11, 19) + ' UTC'
 
     document.getElementById('current-time').innerHTML = now.toLocaleTimeString()
     document.getElementById('utc-time').innerHTML = utcTime
   }
 
-  function setDate () {
-    const now = new Date()
-    document.getElementById('date').innerHTML = now.toDateString().substr(4)
+  const setDate = () => {
+    const date = new Date()
+    const today = date.toLocaleString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    })
+    document.getElementById('date').innerHTML = today
+    document.getElementById('longdate').innerHTML = date.toDateString()
+
+    return today
   }
 
-  function search () {
+  const replace = (id, data) => {
+    document.getElementById(id).innerHTML = data
+  }
+
+  const setWeather = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${CITY}&appid=${API_KEY}&units=imperial`
+    const response = await fetch(url)
+    if (response.status === 200) {
+      const data = await response.json()
+
+      // Parse data
+      const city = `${data.name}: `
+      const desc = `${data.weather[0].description}`
+      const current = `${Math.round(data.main.temp)}°F`
+      const feels = `${Math.round(data.main.feels_like)}°F`
+      const high = `${Math.round(data.main.temp_max)}°F`
+      const low = `${Math.round(data.main.temp_min)}°F`
+      const humidity = `${data.main.humidity}%`
+      const sunriseStamp = new Date(data.sys.sunrise * 1000)
+      const sunrise = `${sunriseStamp.getHours()}`.padStart(2, '0') + `:${sunriseStamp.getMinutes()}`
+      const sunsetStamp = new Date(data.sys.sunset * 1000)
+      const sunset = `${sunsetStamp.getHours()}`.padStart(2, '0') + `:${sunsetStamp.getMinutes()}`
+      const icon = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" width="75px" height="75px">`
+
+      replace('city', city)
+      replace('desc', desc)
+      replace('current', current)
+      replace('feels', feels)
+      replace('high', high)
+      replace('low', low)
+      replace('humidity', humidity)
+      replace('sunrise', sunrise)
+      replace('sunset', sunset)
+      replace('icon', icon)
+
+      return sunriseStamp.getHours(), sunsetStamp.getHours()
+    } else {
+      console.log(response)
+    }
+  }
+
+  const search = () => {
     const str = document.getElementById('search').value
 
     const output = 'https://www.startpage.com/do/dsearch?query=' + str
@@ -32,14 +78,30 @@ document.addEventListener('DOMContentLoaded', function (event) {
     location.href = output
   }
 
-  function setStyle () {
-    const now = new Date()
-    const h = now.getHours()
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const w = now.getDay()
-    const day = days[w]
+  function ColorLuminance (hex, lum) {
+    // https://www.sitepoint.com/javascript-generate-lighter-darker-color/
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '')
+    if (hex.length < 6) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+    }
+    lum = lum || 0
 
-    const greetings = document.querySelector('.content-greeting > h1')
+    // convert to decimal and change luminosity
+    let rgb = '#'
+    let c, i
+    for (i = 0; i < 3; i++) {
+      c = parseInt(hex.substr(i * 2, 2), 16)
+      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16)
+      rgb += ('00' + c).substr(c.length)
+    }
+
+    return rgb
+  }
+
+  const setStyle = (riseHour, setHour, today) => {
+    const now = new Date()
+    let h = now.getHours()
 
     let period
 
@@ -49,43 +111,66 @@ document.addEventListener('DOMContentLoaded', function (event) {
     else period = 0
 
     if (period === 1) {
-      greetings.innerHTML = 'Good morning. It\'s ' + day + '.'
-      document.querySelector(':root').style.setProperty('--background', '#DCEDFF')
-      document.querySelector(':root').style.setProperty('--input', '#fff')
-      document.querySelector(':root').style.setProperty('--link', '#4A5E6D')
-      document.querySelector(':root').style.setProperty('--shadow', '#232C33')
-      document.querySelector(':root').style.setProperty('--color', '#232C33')
+      replace('greeting', 'Good morning. It\'s ')
     }
     if (period === 2) {
-      greetings.innerHTML = 'Good afternoon. It\'s ' + day + '.'
-      document.querySelector(':root').style.setProperty('--background', '#97C3B6')
-      document.querySelector(':root').style.setProperty('--input', '#F9E7E7')
-      document.querySelector(':root').style.setProperty('--color', '#0A2E36')
-      document.querySelector(':root').style.setProperty('--shadow', '#0A2E36')
+      replace('greeting', 'Good afternoon. It\'s ')
     }
     if (period === 3) {
-      greetings.innerHTML = 'Good evening. It\'s still ' + day + '.'
-      document.querySelector(':root').style.setProperty('--background', '#161212')
-      document.querySelector(':root').style.setProperty('--input', '#7B6565')
-      document.querySelector(':root').style.setProperty('--color', '#F9E7E7')
-      document.querySelector(':root').style.setProperty('--link', '#7B6565')
-      document.querySelector(':root').style.setProperty('--hover', '#F9E7E7')
-      document.querySelector(':root').style.setProperty('--shadow', '#000')
+      replace('greeting', 'Good evening. It\'s still ')
     }
     if (period === 0) {
-      greetings.innerHTML = 'It\'s ' + day + ' already. Go get some sleep!'
-      document.querySelector(':root').style.setProperty('--background', '#0C0606')
-      document.querySelector(':root').style.setProperty('--input', '#755')
-      document.querySelector(':root').style.setProperty('--color', '#EDD')
-      document.querySelector(':root').style.setProperty('--link', '#755')
-      document.querySelector(':root').style.setProperty('--hover', '#EDD')
-      document.querySelector(':root').style.setProperty('--shadow', '#000')
+      replace('greeting', 'Go get some sleep! It\'s already ')
+    }
+
+    // Set colors based on the hour
+    let hour = h // Redeclare for testing
+    if (hour === riseHour) {
+      // Set sunrise colors
+      document.querySelector(':root').style.setProperty('--background', '#c9cde5')
+      document.querySelector(':root').style.setProperty('--weather', ColorLuminance('#c9cde5', 0.06))
+      document.querySelector(':root').style.setProperty('--input', '#e3d6ce')
+      document.querySelector(':root').style.setProperty('--link', '#9db2c5')
+      document.querySelector(':root').style.setProperty('--shadow', '#e9e5dc')
+      document.querySelector(':root').style.setProperty('--color', '#445b6d')
+    }
+    if (hour > riseHour) {
+      // Set daytime colors
+      document.querySelector(':root').style.setProperty('--background', '#e8e8e8')
+      document.querySelector(':root').style.setProperty('--weather', ColorLuminance('#e8e8e8', 0.02))
+      document.querySelector(':root').style.setProperty('--input', '#f8f8f8')
+      document.querySelector(':root').style.setProperty('--link', '#788571')
+      document.querySelector(':root').style.setProperty('--shadow', '#c79c72')
+      document.querySelector(':root').style.setProperty('--color', '#222222')
+    }
+    if (hour === setHour) {
+      // Set sunset colors 
+      document.querySelector(':root').style.setProperty('--background', '#6e7382')
+      document.querySelector(':root').style.setProperty('--weather', ColorLuminance('#6e7382', 0.09))
+      document.querySelector(':root').style.setProperty('--input', '#e7ebee')
+      document.querySelector(':root').style.setProperty('--link', '#ccc4b9')
+      document.querySelector(':root').style.setProperty('--shadow', '#535240')
+      document.querySelector(':root').style.setProperty('--color', '#222222')
+    }
+    if (hour > setHour) {
+      // Set nighttime colors
+      document.querySelector(':root').style.setProperty('--background', '#091008')
+      document.querySelector(':root').style.setProperty('--weather', ColorLuminance('#091008', 0.8))
+      document.querySelector(':root').style.setProperty('--input', '#3e563e')
+      document.querySelector(':root').style.setProperty('--link', '#93a889')
+      document.querySelector(':root').style.setProperty('--shadow', '#1f2f22')
+      document.querySelector(':root').style.setProperty('--color', '#ced9df')
     }
   }
-})
+  setTime()
+  let today = setDate()
+  setInterval(setTime, 1000)
+  let riseHour, setHour = setWeather()
+  setStyle(riseHour, setHour, today)
+  const button = document.querySelector('#search-button')
+  button.addEventListener('click', search)
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js');
-  });
-}
+  document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') search()
+  })
+})
